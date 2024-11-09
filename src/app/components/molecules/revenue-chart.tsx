3514133b220b5579d504b92cd942bc79753f4cf3
zarawiicu <zarasaa05@gmail.com>
2024-11-09 23:06:19 +0700

    Memperbaiki Diagram Revenue

--- src/app/components/molecules/revenue-chart.tsx
+++ src/app/components/molecules/revenue-chart.tsx
@@ -25,11 +25,11 @@ export default async function RevenueChart() {
                 Recent Revenue
             </h2>
             <div className="rounded-xl bg-gray-50 p-4">
-                <div className="mt-0 grid grid-cols-12 items-end gap-2 rounded-md bg-white p-4 sm:grid-cols-13 md:gap-4">
+                <div className="mt-0 flex flex-row grid-cols-12 items-end gap-3 rounded-md bg-white p-4 sm:grid-cols-12 md:gap-1">
                     {/* y-axis */}
                     <div
                         className="mb-6 hidden flex-col justify-between text-sm text-gray-400 sm:flex"
-                        style={{ height: `${chartHeight}px` }}
+                        style={{ height: `${chartHeight}px`}}
                     >
                         {yAxisLabels.map((label) => (
                             <p key={label}>{label}</p>
@@ -37,12 +37,12 @@ export default async function RevenueChart() {
                     </div>
 
                     {revenue.map((month) => (
-                        <div key={month.month} className="flex flex-col items-center gap-2">
+                        <div key={month.month} className="flex flex-col  justify-betweenitems-center gap-3 ml-3">
                             {/* bars */}
                             <div
                                 className="w-full rounded-md bg-blue-300"
                                 style={{
-                                    height: `${(chartHeight / topLabel) * month.revenue}px`,
+                                    height: `${(chartHeight / topLabel) * month.revenue}px`, 
                                 }}
                             ></div>
                             {/* x-axis */}
