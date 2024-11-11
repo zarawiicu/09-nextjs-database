a08d2a1341b9a8d5ecdf1831bb315a0e04ebb5e9
zarawiicu <zarasaa05@gmail.com>
2024-11-12 05:44:11 +0700

    Memperbaiki perulangan foto (tapi belum bisa)

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack: (config) => {
+        config.cache = false;
+        return config;
+      },
+};
 
 export default nextConfig;
+  
