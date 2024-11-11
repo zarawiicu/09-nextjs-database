1bf27f3c7bc11b42fc197003a46161c94f093812
zarawiicu <zarasaa05@gmail.com>
2024-11-11 12:25:53 +0700

    Perbaiki foto 1

--- src/seeder/data.js
+++ src/seeder/data.js
@@ -12,10 +12,10 @@ const users = [
       id: '3958dc9e-712f-4377-85e9-fec4b6a6442a',
       name: 'Delba de Oliveira',
       email: 'delba@oliveira.com',
-      image_url: '/customers/pp1.jpg',
+      image_url: '/customers/delba-de-oliveira.png',
     },
     {
-      
+
       id: '3958dc9e-742f-4377-85e9-fec4b6a6442a',
       name: 'Lee Robinson',
       email: 'lee@robinson.com',
